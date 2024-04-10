1f5ba82a42e4877908971a37be483ba12728143f
Folareen <sakawahab03@gmail.com>
2024-04-10 23:02:19 +0100

    fixedd

--- src/routes/ProvidersRoutes.tsx
+++ src/routes/ProvidersRoutes.tsx
@@ -2,20 +2,28 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 // import Login from '../pages/providers/auth/Login'
 import Dashboard from '../pages/providers/main/Dashboard'
+import Login from '../pages/providers/auth/Login'
+import AllServices from '../pages/providers/AllServices'
+import AddService from '../pages/providers/AddService'
+import Orders from '../pages/providers/Orders'
+import Chat from '../pages/providers/Chat'
+import MyRating from '../pages/providers/MyRating'
+import InvoicePage from '../pages/users/Invoice'
+import CreateInvoice from '../pages/providers/CreateInvoice'
 
 const ProvidersRoutes = () => {
     return (
         <Routes>
-            {/* <Route path="/providers/login" element={<Login />} /> */}
+            <Route path="/providers/login" element={<Login />} />
             <Route path="/providers/dashboard" element={<Dashboard />} />
-            <Route path="/providers/my-service" element={<Dashboard />} />
-            <Route path="/providers/add-service" element={<Dashboard />} />
-            <Route path="/providers/orders" element={<Dashboard />} />
-            <Route path="/providers/chat" element={<Dashboard />} />
-            <Route path="/providers/customers" element={<Dashboard />} />
-            <Route path="/providers/my-rating" element={<Dashboard />} />
-            <Route path="/providers/my-invoices" element={<Dashboard />} />
-            <Route path="/providers/create-invoice" element={<Dashboard />} />
+            <Route path="/providers/my-service" element={<AllServices />} />
+            <Route path="/providers/add-service" element={<AddService />} />
+            <Route path="/providers/orders" element={<Orders />} />
+            <Route path="/providers/chat" element={<Chat />} />
+            {/* <Route path="/providers/customers" element={<Customers />} /> */}
+            <Route path="/providers/my-rating" element={<MyRating />} />
+            <Route path="/providers/my-invoices" element={<InvoicePage />} />
+            <Route path="/providers/create-invoice" element={<CreateInvoice />} />
             <Route path="/providers/accounting" element={<Dashboard />} />
             <Route path="/providers/settings" element={<Dashboard />} />
 
